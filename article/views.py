c87d9ae0ace35a899b0e6d9ebe1b61d401400b41
pengyi <535719197@qq.com>
2018-12-07 13:52:00 +0800

    增加article应用

---
+++ article/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404, render_to_response
+
+# Create your views here.
+from django.http import HttpResponse, Http404
+from .models import Article
+
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    context = {
+        'article_obj': article
+    }
+    return render(request, 'article_detail.html', context)
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render_to_response('article_list.html', context)
