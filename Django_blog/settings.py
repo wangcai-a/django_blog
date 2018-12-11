77325f0b568417cc105dab67d3e8993668592d1e
pengyi <535719197@qq.com>
2018-12-11 15:00:50 +0800

    修改默认user模型,迁移数据库

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'user.User'
+
 
 # Application definition
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'user',
 ]
 
 MIDDLEWARE = [
