e255cecf224ca0eee49df7f716ce1b6613aadde4
pengyi <535719197@qq.com>
2018-12-12 22:18:17 +0800

    增加bootstrap文件

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -134,6 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Django_blog/static')
+]
 
 # 设置登录跳转页面
 LOGIN_REDIRECT_URL = '/'
