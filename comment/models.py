129d3691fd3b1de9f186f019a090a7c632a6606e
pengyi <535719197@qq.com>
2018-12-24 11:13:43 +0800

    添加comment模型

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255)
+    url = models.URLField(blank=True)
+    text = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    post = models.ForeignKey('blog.Blog', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.text[:20]
