7bbd13d8337110f1b25c2b9040b94b46346897cf
pengyi <535719197@qq.com>
2019-01-08 14:43:58 +0800

    增加readnum模型,迁移数据模型

--- readnum/admin.py
+++ readnum/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ReadNum
 
 # Register your models here.
+
+
+@admin.register(ReadNum)
+class ReadNum(admin.ModelAdmin):
+    list_display = ('id', 'read_num')
