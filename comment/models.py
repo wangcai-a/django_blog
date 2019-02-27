4359ce922a3030cc275c3c967f35d8b5cd301140
pengyi <535719197@qq.com>
2019-02-27 17:29:00 +0800

    评论模型增加父评论标记,区别回复与评论

--- comment/models.py
+++ comment/models.py
@@ -15,5 +15,9 @@ class Comment(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     comment_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
+    root_id = models.IntegerField(default=0)
+    parent_id = models.IntegerField(default=0)
+    reply_name = models.CharField(max_length=50, blank=True)    # 回复的评论或者账号名称
+
     def __str__(self):
         return self.text[:20]
