b9b2b0926354456c705530056e22a4137459bdf1
pengyi <535719197@qq.com>
2019-03-03 21:53:59 +0800

    修改评论回复模型

--- comment/models.py
+++ comment/models.py
@@ -13,11 +13,11 @@ class Comment(models.Model):
 
     text = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
-    comment_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    comment_user = models.ForeignKey(User, related_name="comments",on_delete=models.DO_NOTHING)
 
     root_id = models.IntegerField(default=0)
-    parent_id = models.IntegerField(default=0)
-    reply_name = models.CharField(max_length=50, blank=True)    # 回复的评论或者账号名称
+    parent = models.ForeignKey('self', null=True, on_delete=models.DO_NOTHING)
+    reply_to = models.ForeignKey(User, null=True, related_name='replies', on_delete=models.DO_NOTHING)    # 回复的评论或者账号名称
 
     def __str__(self):
         return self.text[:20]
